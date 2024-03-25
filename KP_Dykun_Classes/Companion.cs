4873e5c4cf7c69434a07a4ba2b33c52da272af38
Diana Dykun <diana.dykoon@gmail.com>
2024-03-25 12:00:43 +0200

    Реалізувала клас 'Companion'

--- KP_Dykun_Classes/Companion.cs
+++ KP_Dykun_Classes/Companion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace KP_Dykun_Classes
@@ -16,81 +17,156 @@ namespace KP_Dykun_Classes
 
         public override string Login
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get => _login;
+            set => _login = value;
         }
 
         public override string Password
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get => _password;
+            set => _password = value;
         }
 
         public string PhoneNumber
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get => _phoneNumber;
+            set => _phoneNumber = value;
         }
 
         public string Name
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get => _name;
+            set => _name = value;
         }
 
         public byte[] Photo
         {
-            get => throw new NotImplementedException();
-            set => throw new NotImplementedException();
+            get => _photo;
+            set => _photo = value;
         }
 
         public Companion(string login, string password, string phoneNumber, string name)
         {
+            _login = login;
+            _password = password;
+            _phoneNumber = phoneNumber;
+            _name = name;
+
         }
 
         public override bool Authorization(string login, string password, List<User> users)
         {
-            throw new NotImplementedException();
+            var p = users.Where(s => s.Login == login).ToList();
+            if (p.Count == 0)
+            {
+                return false;
+            }
+            var k = users.Where(s => s.Login == login && s.Password == password).ToList();
+            if (k.Count == 0)
+                return false;
+            return true;
         }
 
         public bool Registration(string login, string password, string name, string phoneNumber)
         {
-            throw new NotImplementedException();
+            string pattern = @"^\+38\(\d{3}\)-\d{7}$";
+            if (login.Length < 4 || password.Length < 7 || string.IsNullOrEmpty(name))
+                return false;
+
+            if (!Regex.IsMatch(phoneNumber, pattern))
+                return false;
+
+            return true;
         }
 
         public bool SearchTrip(DateTime date, short numberOfSeatsRequired, string pointOfDeparture, string destination)
         {
-            throw new NotImplementedException();
+            if (date < DateTime.Now || numberOfSeatsRequired < 1)
+                return false;
+
+            if (string.IsNullOrEmpty(pointOfDeparture) || string.IsNullOrEmpty(destination))
+                return false;
+
+            return true;
         }
 
         public bool BookPlace(int tripNumber, short numberOfSeatsRequired, DateTime date, List<Trip> trips)
         {
-            throw new NotImplementedException();
+            var trip = trips.Where(t => t.Number == tripNumber).FirstOrDefault();
+            if (trip is null)
+                return false;
+
+            if (numberOfSeatsRequired > trip.NumberOfSeats || date != trip.Date)
+                return false;
+
+            return true;
         }
 
         public List<Trip> ViewTripHistory(string login, List<Trip> trips)
         {
-            throw new NotImplementedException();
+            List<Trip> companionTrip = new List<Trip>();
+            foreach (var trip in trips)
+            {
+                foreach (var comTrip in trip.companions)
+                    if (comTrip.Login == login)
+                        companionTrip.Add(trip);
+            }
+            return companionTrip;
         }
 
         public bool RateDriver(int numberOfTrip, Driver driver, int grade, List<Trip> trips)
         {
-            throw new NotImplementedException();
+            if (grade < 0)
+                return false;
+            var trip = trips.Where(s => s.Number == numberOfTrip).FirstOrDefault();
+
+            if (trip is null)
+                return false;
+
+            if (trip.Driver != driver)
+                return false;
+
+            bool companionExist = false;
+            foreach (var companion in trip.companions)
+            {
+                if (companion.Login == Login)
+                {
+                    companionExist = true;
+                    break;
+                }
+            }
+
+            return companionExist;
         }
 
         public bool LoadPhoto(string filePath)
         {
-            throw new NotImplementedException();
+            if (!File.Exists(filePath))
+                return false;
+            string[] allowedFormat = { ".bmp", ".jpg" };
+            string extension = Path.GetExtension(filePath).ToLower();
+            if (!allowedFormat.Contains(extension))
+                return false;
+
+            Photo = File.ReadAllBytes(filePath);
+            return true;
         }
 
         public bool UpdateProfile(string password, string name, string phoneNumber)
         {
-            throw new NotImplementedException();
+            string pattern = @"^\+38\(\d{3}\)-\d{7}$";
+            if (password.Length < 7 || string.IsNullOrEmpty(name))
+                return false;
+
+            if (!Regex.IsMatch(phoneNumber, pattern))
+                return false;
+
+            return true;
         }
 
         public string CompanionInfo()
         {
-            throw new NotImplementedException();
+            return $"{Login}, {Name}, {PhoneNumber}";
         }
     }
 
